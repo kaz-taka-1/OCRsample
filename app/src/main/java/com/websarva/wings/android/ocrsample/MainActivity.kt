4754f7b59efea35a6cc75b40009f019ae7ea3a1a
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2022-10-26 23:30:14 +0900

    PERMISSIONtest

--- app/src/main/java/com/websarva/wings/android/ocrsample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/ocrsample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
@@ -20,6 +21,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
@@ -33,80 +35,101 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btClick = findViewById<Button>(R.id.btOutput)
-        val listener = OCROutListenner()
-
-        btClick.setOnClickListener(listener)
+        // 端末の画像を取得する
+        loadImages()
     }
-    private inner class OCROutListenner : View.OnClickListener{
-        override fun onClick(view: View){
-            val items = mutableListOf<String>()
-            val collection =
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    MediaStore.Images.Media.getContentUri(
-                       MediaStore.VOLUME_EXTERNAL_PRIMARY
-                    )
-                } else {
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                }
-            val projection = arrayOf(
-                MediaStore.Images.Media._ID,
-                MediaStore.Images.Media.DISPLAY_NAME,
-                MediaStore.Images.Media.DATE_TAKEN
-            )
-            val selection = null
 
-            applicationContext.contentResolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                projection,
-                selection,
-                null,
-                null
-            )?.use { cursor ->
-                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
-                while (cursor.moveToNext()) {
-                    val id = cursor.getLong(idColumn)
-                    val contentUri: Uri = ContentUris.withAppendedId(
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                        id
-                    )
-                    println(contentUri)
-                    items.add(contentUri.toString())
-                }
+    // requestPermissionsの後に呼び出される
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != REQUEST_CODE_PERMISSION) return
 
-            }//Uriget
-
-            var output = findViewById<TextView>(R.id.OCROutput)
-            output.text="test"
-            println(items.count())
-            println(collection)
-            println(projection)
-
-            /*var image:FirebaseVisionImage? =null
-            try {
-                image = FirebaseVisionImage.fromFilePath(this@MainActivity, uri)
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }//uriから画像を取り込む
-
-
-
-            val detector = FirebaseVision.getInstance()
-                .onDeviceTextRecognizer
-            if(image != null){
-                val result = detector.processImage(image)
-                    .addOnSuccessListener { firebaseVisionText ->
-                        val OCRresult = processTextBlock(firebaseVisionText)
-                        output.text = OCRresult
-                    }
-                    .addOnFailureListener { e ->
-                        output.text = "OCR失敗"
-                    }
-            }else{
-                output.text = "imageが読み込めませんでした"
-            }*/
+        if (grantResults.isNotEmpty() && (grantResults.first() == PackageManager.PERMISSION_GRANTED)) {
+            // 許可した後の処理
         }
     }
+
+    private fun loadImages() {
+
+        // READ_EXTERNAL_STORAGEパーミッションが許可されているかチェックする
+        val REQUEST_CODE_PERMISSION = 1
+        val isGranted = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+        // 許可されていない場合、パーミッションの許可を選択するダイアログを表示する
+        if (!isGranted) {
+            requestPermissions(
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_CODE_PERMISSION
+            )
+            return
+        }
+
+        val uris = mutableListOf<Uri>()
+
+        // 外部ストレージを指定する
+        val queryUri: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        // queryで取得したいカラムを指定する
+        val projection: Array<String> = arrayOf(MediaStore.Images.Media._ID)
+
+        /**
+         * 検索条件を指定する。
+         * 条件は「"${MediaStore.Images.Media.DATE_TAKEN} >= ?"」のように記述する。
+         * 全件取得のときはnullを指定する。
+         */
+        val selection: String? = null
+
+        /**
+         * selectionの「?」に代入する条件を指定する。
+         * 全件取得のときはnullを指定する。
+         */
+        val selectionArgs: Array<String>? = null
+
+        /**
+         * ソート条件を指定する。
+         * 条件は「"${MediaStore.Images.Media.DATE_ADDED} DESC"」のように記述する。
+         * 条件を指定しない場合はnullを設定する。
+         */
+        val sortOrder: String? = null
+
+        contentResolver.query(
+            queryUri,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        ).use { cursor ->
+
+            cursor ?: return@use
+
+            // projectionで指定していないカラムを取得しようとすると、例外がスローされる。
+            val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+
+            while (cursor.moveToNext()) {
+
+                val imageId = cursor.getLong(columnIndex)
+
+                // メディアアイテムのURIを取得する
+                val uri = Uri.withAppendedPath(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    imageId.toString()
+                )
+
+                uris.add(uri)
+            }
+        }
+    }
+
+
+
+
 
     private fun processTextBlock(result:FirebaseVisionText): String {
         val resultText = result.text
